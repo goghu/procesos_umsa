fa06bd52d736863aa78c26e5a1a37b4e3bbda53f
Cristiam Herrera <cristiamherrera@gmail.com>
2018-06-02 17:55:33 -0400

    Se adiciono un datatable a personas

--- app/datatables/persona_datatable.rb
+++ app/datatables/persona_datatable.rb
@@ -1,12 +1,14 @@
 class PersonaDatatable < AjaxDatatablesRails::Base
 
+  def_delegators :@view, :options_for_select, :h, :productos_path
+
   def view_columns
     # Declare strings in this format: ModelName.column_name
     # or in aliased_join_table.column_name format
     @view_columns ||= {
       id: { source: "Persona.id", cond: :eq,  searchable: false },
       ci: { source: "Persona.ci", cond: :like },
-      nombre: { source: "Persona.nombre", cond: :like, searchable: false }
+      nombre: { source: "Persona.nombre", cond: :like }
     }
   end
 
@@ -16,7 +18,8 @@ class PersonaDatatable < AjaxDatatablesRails::Base
         # example:
         id: record.id,
         ci: record.ci,
-        nombre: record.nombre
+        nombre: record.nombre,
+        acciones: dt_actions(record.id)
       }
     end
   end
@@ -26,9 +29,10 @@ class PersonaDatatable < AjaxDatatablesRails::Base
     Persona.select("personas.id, personas.ci, personas.nombre")
   end
 
-  def dt_actions
+  def dt_actions(id_persona)
     # links = []
-    links = "enlace"
+    # links = "enlace"+id_persona.to_s
+    links = '<button type="button" id="bt_sel" class="btn btn-success"><i class="mdi mdi-check"></i></button>'.html_safe
     # links << h.link_to 'Edit', h.edit_user_path(object) if h.policy(object).update?
     # links << h.link_to 'Delete', h.user_path(object), method: :delete, remote: true if h.policy(object).destroy?
     # h.safe_join(links, '')
